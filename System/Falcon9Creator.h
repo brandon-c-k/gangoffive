1d6cd39bbc9dfc5c527db6dd327c672dbe76c2fc
MaverickGDN0403 <83560317+MaverickGDN0403@users.noreply.github.com>
2021-11-18 16:33:03 +0200

    Updated Abstract Factory classes

--- System/Falcon9Creator.h
+++ System/Falcon9Creator.h
@@ -2,9 +2,12 @@
 #define GANGOFFIVE_FALCON9CREATOR_H
 
 #include "RocketCreator.h"
+#include "Falcon9.h"
+
 
 class Falcon9Creator: public RocketCreator{
-
+public:
+    Rocket* createFalcon9(); 
 };
 
 
